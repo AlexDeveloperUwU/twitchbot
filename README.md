447e5363831c77dac3a1ab5630900647d8355aaf
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2023-01-12 22:50:15 +0100

    app start

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Test de integración de una página web con un bot de Twitch
 La idea aquí es conectar una página web y que dependiendo de los comandos que ejecuten los usuarios del chat renderice una cosa u otra.
 
 Priariamente será usada como un overlay para el OBS.
-
---
+++ index.js
@@ -0,0 +1,43 @@
+const tmi = require('tmi.js');
+const express = require('express')
+const app = express()
+const http = require('http')
+const Enmap = require('enmap');
+
+app.set('port', process.env.PORT || 80)
+
+app.use(express.json())
+
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.set("view engine", "ejs")
+
+app.use('/static', express.static(__dirname + '/files'));
+
+app.set("views", require("path").join(__dirname, "views"))
+
+app.db = new Enmap({
+    name: 'database'
+})
+
+app.db.set("parametros", "null")
+
+app.get('*', function (req, res) {
+    const parameters = app.db.get("parametros")
+    if (parameters === "null") {
+        res.render("index", {
+            params: parameters
+        })
+    } else {
+        res.render("index", {
+            params: parameters
+        })
+    }
+});
+
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
